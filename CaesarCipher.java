e7be2992fbfa20895cb8430c2f80ff4a88ee8361
Christine Okubo <72178074+cjcokubo@users.noreply.github.com>
2021-03-16 00:19:25 +0800

    Add files via upload

---
+++ CaesarCipher.java
@@ -0,0 +1,70 @@
+package question2;
+
+/* Encryption with the Caesar Cipher */
+/* Goodrich, Tamassia, Goldwasser */
+
+
+public class CaesarCipher{
+
+	protected char[] encoder = new char[26]; // Encryption Array 
+	protected char[] decoder = new char[26]; // Decryption array
+
+	/* The constructor initializes the  encryption and decryption arrays*/
+
+	public CaesarCipher(int rotation){
+
+		for(int k=0; k<26; k++){
+
+			encoder[k] = (char)('A' + (k+rotation)%26);
+			decoder[k] = (char)('A' + (k-rotation + 26)%26);
+
+		}
+	}
+
+	public String encrypt(String message){
+
+		/*return String representing the encrypted message */
+		return transform(message, encoder);
+
+	}
+
+
+	public String decrypt(String secret){
+
+		return transform(secret, decoder); 
+
+	}
+
+	private String transform(String original, char[] code){
+
+		char[] msg = original.toCharArray();
+		for(int k=0; k< msg.length; k++){
+
+			if(Character.isUpperCase(msg[k])){
+
+				int j = msg[k] - 'A';
+				msg[k] = code[j];
+
+			}
+			//return new String(msg);
+		}
+		return new String(msg);
+	}
+
+	public static void main(String[] args){
+		/*main method for testing the Caesar Cipher*/
+
+
+		CaesarCipher cipher = new CaesarCipher(3);
+		System.out.println("Encryption code = "+ new String(cipher.encoder));
+		System.out.println("Decryption code = "+ new String(cipher.decoder));
+		String message = "THE EAGLE IS IN PLAY; MEET AT JOE'S";
+		String coded = cipher.encrypt(message);
+		System.out.println("Secret: "+ coded);
+		String answer = cipher.decrypt(coded);
+		System.out.println("Message: " + answer);
+
+	}
+
+
+}
